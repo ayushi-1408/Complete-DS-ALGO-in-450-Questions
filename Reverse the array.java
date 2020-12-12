9c98a0ea4056a645ea0a6a023cd6d91f4edd89b5
AYUSHI TYAGI <60479969+ayushi-1408@users.noreply.github.com>
2020-12-12 17:35:53 +0530

    Update Reverse the array.java

--- Reverse the array.java
+++ Reverse the array.java
@@ -6,7 +6,7 @@ public class Practice2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the rating ");
+		System.out.println("enter the n ");
 		int n = sc.nextInt();
 		int i,j;
 		int[] a= new int[n];
